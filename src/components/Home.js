70d31c156a06ccba447a9d758984e5de73458a93
b-seung <131140437+b-seung@users.noreply.github.com>
2023-04-25 22:43:57 +0900

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,40 @@
 import { useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 import { useEffect } from "react";
+import { chatTableTest, friendsTableTest, loginTableTest } from "../api/test";
+import {MdKeyboardArrowDown, MdKeyboardArrowUp} from 'react-icons/md';
 
-const FriendItem = () => {};
+const FriendItem = ({nickname}) => {
+  return (
+    <>
+      <div className="image"></div>
+      <div className="name">{nickname}</div>
+    </>
+  )
+};
 
-const TalkItem = () => {};
+const TalkItem = ({chat}) => {
+  const {myId, yourId, text, datetime} = chat;
+  return (
+    <>
+      <div className="image"></div>
+      <div className="mid">
+        <div className="name">{nickname}</div>
+        <div className="text">{text}</div>
+      </div>
+      <div className="datetime">
+        <div className="date">{date}</div>
+        <div className="time">{time}</div>
+      </div>
+    </>
+  )
+};
 
 const Home = ({ loginId }) => {
   const navigate = useNavigate();
+
+  const [openFriends, setOpenFriends] = useState(true);
+  const [openChat, setOpenChat] = useState(true);
 
   useEffect(() => {
     if (loginId === null) {
@@ -15,10 +42,30 @@ const Home = ({ loginId }) => {
     }
   });
 
-  const onClick = () => {
-    navigate("./login");
-  };
-  return <div onClick={onClick}>home</div>;
+  const friendsList = friendsTableTest.getFriendsList(loginTableTest.test_login_id);
+  const chatsList = chatTableTest.getChatList(loginTableTest.test_login_id);
+
+  return (
+  <div>
+    <div>
+    <div>友達リスト</div>
+    {openFriends ? <MdKeyboardArrowDown /> : <MdKeyboardArrowUp />}
+    </div>
+    <div>
+      {friendsList.forEach(friend => {
+        return <FriendItem nickname={friend}></FriendItem>
+      })}
+    </div>
+    <div>
+    <div>チャットリスト</div>
+    {openChat ? <MdKeyboardArrowDown /> : <MdKeyboardArrowUp />}
+    </div>
+    <div>
+      {chatsList.forEach(chat => {
+        return <TalkItem chat={chat}></TalkItem> 
+      })}
+    </div>
+  </div>);
 };
 
 export default connect(({ login }) => ({ loginId: login.id }))(Home);
