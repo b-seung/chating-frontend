f93ac08436be1c7bd798117b0c9ceede19483507
b-seung <131140437+b-seung@users.noreply.github.com>
2023-04-25 22:43:28 +0900

    Update test.js

--- src/api/test.js
+++ src/api/test.js
@@ -1,8 +1,8 @@
-function TestCase() {
-  this.test_id = ["test1"];
-  this.test_password = ["test1"];
-  this.test_nickname = ["test1"];
-  this.test_birthday = ["2023-04-24"];
+function LoginTable() {
+  this.test_id = ["test1", "test2", "test3"];
+  this.test_password = ["test1", "test2", "test3"];
+  this.test_nickname = ["test1", "test2", "test3"];
+  this.test_birthday = ["2023-04-24", "2023-04-25", "2023-04-26"];
 
   this.test_login_id = "";
 
@@ -54,4 +54,49 @@ function TestCase() {
   };
 }
 
-export const testcase = new TestCase();
+function FriendsTable() {
+  this.myId = ["test1", "test1", "test2", "test3"];
+  this.yourId = ["test2", "test3", "test1", "test1"];
+
+  this.getFriendsList = (loginId) => {
+    const result = [];
+
+    this.myId.forEach(id, index => {
+      if (id === loginId) result.push(this.yourId[index]);
+    })
+
+    return result;
+  }
+}
+
+function ChatsData(id, text, datetime) {
+  this.id = id;
+  this.text = text;
+  this.datetime = datetime;
+}
+
+function Chat(id, text, )
+
+function ChatTable() {
+  this.chatList = new Array();
+
+  this.chatList.push(new Chat("test1", "test2", "おはよう", "2023-04-25 17:19:52"));
+  this.chatList.push(new Chat("test1", "test2", "こんにちは", "2023-04-25 17:20:14"));
+  this.chatList.push(new Chat("test1", "test3", "こんばんは", "2023-04-25 17:20:55"));
+  this.chatList.push(new Chat("test2", "test1", "いただきます", "2023-04-25 17:21:17"));
+
+  this.getChatList = (loginId) => {
+    const chats = new Map();
+
+    this.chatList.forEach(chat=> {
+      if (chat.myId === loginId || chat.yourId === loginId) ;
+    })
+
+    chats.sort(chat1, chat2 => {return chat2.datetime - char1.datetime});
+    return chats;
+  }
+}
+
+export const loginTableTest = new LoginTable();
+export const friendsTableTest = new FriendsTable();
+export const chatTableTest = new ChatTable();
